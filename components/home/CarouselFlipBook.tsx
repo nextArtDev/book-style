f0821f32a2ec4224448a740da313c3d3e363ec15
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2024-03-09 17:48:24 +0330

    carousel style

--- components/home/CarouselFlipBook.tsx
+++ components/home/CarouselFlipBook.tsx
@@ -2,7 +2,7 @@
 import { cn } from '@/lib/utils'
 import React, { ReactNode, useEffect, useRef, useState } from 'react'
 import HTMLFlipBook from 'react-pageflip'
-
+import style from './card-parallax/style.module.scss'
 import { CategoryFullStructure } from '@/lib/queries/home/category'
 import { FC } from 'react'
 
@@ -90,7 +90,7 @@ const CarouselFlipBook: FC<CarouselFlipBookProps> = ({ categories }) => {
             // bg-[#fefefe]
             className="photocopied relative border border-[#c2b5a3] grid grid-cols-2 dark:bg-light-bg bg-dark-bg "
           >
-            <div className=" relative p-2 overflow-hidden w-full h-1/2">
+            <div className=" relative p-2 overflow-hidden w-full h-3/4">
               <Image
                 className="object-cover  rounded-md"
                 fill
@@ -99,7 +99,13 @@ const CarouselFlipBook: FC<CarouselFlipBookProps> = ({ categories }) => {
               />
             </div>
 
-            <div>{category.name}</div>
+            <div className="pt-12 flex items-center justify-center text-center">
+              <div className={cn(' text-xl font-semibold ', style.paperCorner)}>
+                <p className=" text-primary dark:text-secondary ">
+                  {category.name}
+                </p>
+              </div>
+            </div>
           </div>
         ))}
       </HTMLFlipBook>
