56cc12fb2d65b069617edf3f4a097bb4b6cbf409
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2024-03-15 04:10:29 +0330

    fixing routes middleware

--- routes.ts
+++ routes.ts
@@ -3,7 +3,7 @@
  * These routes do not require authentication
  * @type {string[]}
  */
-export const publicRoutes = ['/', '/new-verification']
+export const publicRoutes = ['/']
 
 /**
  * An array of routes that are used for authentication
@@ -24,7 +24,7 @@ export const authRoutes = [
  * Routes that start with this prefix are used for API authentication purposes
  * @type {string}
  */
-export const apiAuthPrefix = '/'
+export const apiAuthPrefix = '/api/auth'
 
 /**
  * The default redirect path after logging in
